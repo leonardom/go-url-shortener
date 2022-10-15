ae219ce7ad6d4184a8f4d159b32ce33e34a4eb0f
Leonardo Marcelino <leonardo.marcelino@goodlpay.com>
2022-10-15 13:02:03 -0300

    Business logic implementation

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main()  {
+	
+}
+
+// https://www.google.com -> 98sj1-293
+// https://localhost.com:8000/98sj1-293 -> https://www.google.com
+
+// repos <- service -> serializer -> http
